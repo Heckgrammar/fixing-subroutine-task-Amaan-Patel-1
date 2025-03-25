1fb91bea0cc711bcb41cb1a7ee5e945d53658d2b
Amaan-Patel-1 <rubixisawizard@gmail.com>
2025-03-25 09:28:25 +0000

    Update Program.cs

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -1,18 +1,30 @@
-﻿namespace FixSubroutineTask
+using System;
+
+namespace FixSubroutineTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
-                Console.WriteLine(diffCurrencies(i));            
+            for (int i = 8; i >= 0; i--)
+            {
+                Console.WriteLine(diffCurrencies(i));
             }
         }
+
         static string diffCurrencies(int x)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
 
+            // Check if x is within the bounds of the currencies array
+            if (x >= 0 && x < currencies.Length)
+            {
+                return currencies[x];
+            }
+            else
+            {
+                return "Invalid currency index"; // Handle out-of-bounds index
+            }
         }
     }
 }
